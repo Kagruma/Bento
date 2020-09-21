a296cffbf08fdd4d71c81cf4a9dc5eaaae540f43
Kagruma <33601400+Kagruma@users.noreply.github.com>
2020-09-21 16:57:26 -0400

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -2,11 +2,11 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'Evelyn';
-var lateTxt = 'Go to Sleep! ';
-var morningTxt = 'Good morning, ';
-var afterTxt = 'Good afternoon, ';
-var evenTxt = 'Good evening, ';
+var name = 'evelyn';
+var lateTxt = 'go to bed! ';
+var morningTxt = 'mornin! ';
+var afterTxt = 'afternoon!, ';
+var evenTxt = 'evening!, ';
 
 if (Hr >= 0 && Hr < 6) {
     document.getElementById('greetings').innerText = lateTxt + name;
